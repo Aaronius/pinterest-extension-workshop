8b1290cd7367d4972fc6c234195e600b64efd29c
Aaron Hardy <aahardy@adobe.com>
2019-01-22 14:18:40 -0700

    Added load library code.

--- src/lib/actions/loadLibrary.js
+++ src/lib/actions/loadLibrary.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = function(settings) {
-  // TODO Perform some action.
+module.exports = function() {
+  if (!window.pintrk) {
+    var scriptUrl = "https://s.pinimg.com/ct/core.js";
+    window.pintrk = function() {
+      window.pintrk.queue.push(Array.prototype.slice.call(arguments));
+    };
+    var pintrk = window.pintrk;
+    pintrk.queue = [];
+    pintrk.version = "3.0";
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptUrl;
+    var firstScriptInPage = document.getElementsByTagName("script")[0];
+    firstScriptInPage.parentNode.insertBefore(script, firstScriptInPage);
+    pintrk("load", "YOUR_TAG_ID");
+    pintrk("page");
+  }
 };
